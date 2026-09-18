e83f2abb63ab8dc61f31c8be13450fe581d6add5
phtn <phtn458@gmail.com>
2026-09-18 21:38:56 +0800

    update:email-recipient

--- src/lib/icons/icons.ts
+++ src/lib/icons/icons.ts
@@ -648,7 +648,7 @@ export const icons = {
   },
   link: {
     symbol:
-      '<g transform="scale(0.667)"><path fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" d="m9 15 6-6m-4-3 .463-.536a5 5 0 0 1 7.071 7.072L18 13m-5 5-.397.534a5.07 5.07 0 0 1-7.127 0 4.97 4.97 0 0 1 0-7.071L6 11"/></g>',
+      '<g transform="scale(0.667)"><path fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.75" d="m9 15 6-6m-4-3 .463-.536a5 5 0 0 1 7.071 7.072L18 13m-5 5-.397.534a5.07 5.07 0 0 1-7.127 0 4.97 4.97 0 0 1 0-7.071L6 11"/></g>',
     viewBox: '0 0 16 16',
     set: 'svg'
   },
